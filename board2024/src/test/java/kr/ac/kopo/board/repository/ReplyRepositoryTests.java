05a03173c65621e3e48743fe62961da6ee270c2d
S7f5bd8 <103229461+S7f5bd8@users.noreply.github.com>
2024-10-31 12:08:00 +0900

    Add files via upload

---
+++ board2024/src/test/java/kr/ac/kopo/board/repository/ReplyRepositoryTests.java
@@ -0,0 +1,43 @@
+package kr.ac.kopo.board.repository;
+
+import kr.ac.kopo.board.entity.Board;
+import kr.ac.kopo.board.entity.Member;
+import kr.ac.kopo.board.entity.Reply;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class ReplyRepositoryTests {
+    @Autowired
+    private ReplyRepository replyRepository;
+
+    @Test
+    public void testListByBoard(){
+        List<Reply> replyList = replyRepository.getRepliesByBoardOrderByRno(Board.builder().bno(100L).build());
+        replyList.forEach(reply -> System.out.println(reply));
+    }
+
+    @Test
+    public void insertReply(){
+        IntStream.rangeClosed(1, 300).forEach(i ->{
+            long bno = (long) (Math.random() * 100 + 1);//1~100 임의의 long 형의 정수 값
+
+            Board board = Board.builder()
+                    .bno(bno)
+                    .build();
+
+            Reply reply = Reply.builder()
+                    .text("Reply " + i)
+                    .board(board)
+                    .replyer("guest")
+                    .build();
+
+            replyRepository.save(reply);
+        });
+    }
+}
